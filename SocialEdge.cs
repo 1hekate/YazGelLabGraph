3b3712a71dc2413a9ea9fb5bd9f59dcce9fef09c
1hekate <153822310+1hekate@users.noreply.github.com>
2025-12-29 16:12:02 +0300

    Add files via upload

---
+++ SocialEdge.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace YazGelLab
+{
+    public class SocialEdge
+    {
+        public UserNode BaslangicDugumu { get; set; }
+        public UserNode BitisDugumu { get; set; }
+        public double Agirlik { get; private set; } // Otomatik hesaplanacak
+
+        public SocialEdge(UserNode baslangic, UserNode bitis)
+        {
+            BaslangicDugumu = baslangic;
+            BitisDugumu = bitis;
+            AgirlikHesapla();
+        }
+
+        // PDF Madde 4.3'teki Dinamik Ağırlık Hesaplama Formülü
+        private void AgirlikHesapla()
+        {
+            double diffAktiflik = BaslangicDugumu.Aktiflik - BitisDugumu.Aktiflik;
+            double diffEtkilesim = BaslangicDugumu.Etkilesim - BitisDugumu.Etkilesim;
+            double diffBaglanti = BaslangicDugumu.BaglantiSayisi - BitisDugumu.BaglantiSayisi;
+
+            // Formül: 1 + Sqrt((Fark1^2) + (Fark2^2) + (Fark3^2))
+            double karelerToplami = (diffAktiflik * diffAktiflik) +
+                                    (diffEtkilesim * diffEtkilesim) +
+                                    (diffBaglanti * diffBaglanti);
+
+            Agirlik = 1 + Math.Sqrt(karelerToplami);
+        }
+    }
+}
