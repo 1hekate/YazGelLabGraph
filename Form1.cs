1f319dcd307e43199b86dd53c6d5bc741313333e
semihgokmen <semihgokmenn.3452@gmail.com>
2026-01-01 23:50:48 +0300

    form structure

--- Form1.cs
+++ Form1.cs
@@ -12,15 +12,15 @@ namespace YazGelLab
         GraphManager graphManager;
         Random rnd = new Random();
 
-        // Seçim Değişkenleri
-        UserNode seciliDugum = null; // Sol Tık
-        UserNode hedefDugum = null;  // Sağ Tık
+        UserNode seciliDugum = null;
+        UserNode hedefDugum = null;
 
         public Form1()
         {
             InitializeComponent();
             graphManager = new GraphManager();
         }
+
         private void Form1_Load(object sender, EventArgs e)
         {
             if (gridSonuclar != null)
@@ -31,38 +31,5 @@ namespace YazGelLab
                 gridSonuclar.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             }
         }
-        private void pnlGraph_MouseClick(object sender, MouseEventArgs e)
-        {
-            Point tiklananYer = e.Location;
-            bool birineTiklandi = false;
-
-            foreach (var node in graphManager.Nodes)
-            {
-                double mesafe = Math.Sqrt(
-                    Math.Pow(node.Konum.X - tiklananYer.X, 2) +
-                    Math.Pow(node.Konum.Y - tiklananYer.Y, 2)
-                );
-
-                if (mesafe < 20)
-                {
-                    if (e.Button == MouseButtons.Left)
-                        seciliDugum = node;
-                    else if (e.Button == MouseButtons.Right)
-                        hedefDugum = node;
-
-                    birineTiklandi = true;
-                    pnlGraph.Invalidate();
-                    break;
-                }
-            }
-
-            if (!birineTiklandi)
-            {
-                seciliDugum = null;
-                hedefDugum = null;
-                pnlGraph.Invalidate();
-            }
-        }
     }
 }
-    
