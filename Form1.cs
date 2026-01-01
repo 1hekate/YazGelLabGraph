8297899d3cf3a45035e2579d1346c27d09e2d929
semihgokmen <semihgokmenn.3452@gmail.com>
2026-01-01 23:40:49 +0300

    selecting node

--- Form1.cs
+++ Form1.cs
@@ -31,6 +31,38 @@ namespace YazGelLab
                 gridSonuclar.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             }
         }
+        private void pnlGraph_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point tiklananYer = e.Location;
+            bool birineTiklandi = false;
+
+            foreach (var node in graphManager.Nodes)
+            {
+                double mesafe = Math.Sqrt(
+                    Math.Pow(node.Konum.X - tiklananYer.X, 2) +
+                    Math.Pow(node.Konum.Y - tiklananYer.Y, 2)
+                );
+
+                if (mesafe < 20)
+                {
+                    if (e.Button == MouseButtons.Left)
+                        seciliDugum = node;
+                    else if (e.Button == MouseButtons.Right)
+                        hedefDugum = node;
+
+                    birineTiklandi = true;
+                    pnlGraph.Invalidate();
+                    break;
+                }
+            }
+
+            if (!birineTiklandi)
+            {
+                seciliDugum = null;
+                hedefDugum = null;
+                pnlGraph.Invalidate();
+            }
+        }
     }
 }
     
